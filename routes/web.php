df00899e4120f6115dff2df180b5b3df9db3bb46
Maxime <maxime.mrt06@gmail.com>
2021-03-24 21:25:49 +0100

    Add Breeze

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('home');
+
+require __DIR__.'/auth.php';
